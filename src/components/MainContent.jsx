4bba110dc0c15f2c0b0d5584d78f7b4edecfa183
Thibault <51710597+perestroika@users.noreply.github.com>
2021-05-03 13:10:12 +0200

    typo

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,7 +4,7 @@ import Charts from './Charts'
 function MainContent() {
     return (
         <div className="flex-1 font-medium px-9 py-12">
-            <h2 className="text-5xl leading-tight mb-2">Violence Againts Civilians Dashboard</h2>
+            <h2 className="text-5xl leading-tight mb-2">Violence Against Civilians Dashboard</h2>
             <p className="text-brand-light-gray">Data's about intentional attacks on civilians by governments and formally organized armed groups.</p>
             <br/>
             <Charts/>
