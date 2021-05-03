934b5524af57df8fcf76503ada4304b036e86b48
Thibault <51710597+perestroika@users.noreply.github.com>
2021-05-03 13:53:33 +0200

    logo change

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import logo from '../assets/world.png'
+import logo from '../assets/data.svg'
 
 
 function Sidebar(){
     return (
         <div className="w-72 min-h-screen border-r border-opacity-15 px-8">
             <div>
-                <Link to="/"><img src={logo} alt="dashboard logo" className="py-16 mx-auto"/></Link>
+                <Link to="/"><img src={logo} alt="dashboard logo" width={65} height={65} className="py-16 mx-auto"/></Link>
             </div>
 
             <div className="side-menu font-medium leading-5 space-y-1.5">
