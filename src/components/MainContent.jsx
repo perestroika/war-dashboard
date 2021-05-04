e53b8a9bd10327d78c10df541a9630233bc1754b
Thibault <51710597+perestroika@users.noreply.github.com>
2021-05-04 09:44:09 +0200

    sticky title

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,9 +4,11 @@ import Charts2 from "./dashboard-charts/Charts2";
 
 function MainContent() {
     return (
-        <div className="flex-1 font-medium px-9 py-12">
-            <h2 className="text-5xl leading-tight mb-2 ">Violence Against Civilians Dashboard</h2>
-            <p className="text-brand-light-gray">Data's about intentional attacks on civilians by governments and formally organized armed groups.</p>
+        <div className="flex-1">
+            <div className="flex-1 font-medium px-9 py-12 sticky top-0 z-50 bg-white shadow-sm">
+                <h2 className="text-5xl leading-tight mb-2  ">Violence Against Civilians Dashboard</h2>
+                <p className="text-brand-light-gray">Data's about intentional attacks on civilians by governments and formally organized armed groups.</p>
+            </div>
             <br/>
             <Charts/>
             <Charts2/>
