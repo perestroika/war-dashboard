aecc21aa4f360735d8bf9a407ec9c0c89a6caaf9
Thibault <51710597+perestroika@users.noreply.github.com>
2021-05-04 09:49:39 +0200

    h1 weight

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -6,7 +6,7 @@ function MainContent() {
     return (
         <div className="flex-1">
             <div className="flex-1 font-medium px-9 py-12 sticky top-0 z-50 bg-white shadow-sm">
-                <h2 className="text-5xl leading-tight mb-2  ">Violence Against Civilians Dashboard</h2>
+                <h2 className="text-5xl leading-tight mb-2 font-semibold ">Violence Against Civilians Dashboard</h2>
                 <p className="text-brand-light-gray">Data's about intentional attacks on civilians by governments and formally organized armed groups.</p>
             </div>
             <br/>
